ae1232dab38eb6d60186602565a827754f4002d5
lubaskin <lubaskincorporation@gmail.com>
2025-03-01 21:08:45 +0300

    descriptions

--- src/crudik/application/data_model/mentor.py
+++ src/crudik/application/data_model/mentor.py
@@ -1,17 +1,17 @@
 from uuid import UUID
 
 from adaptix.conversion import get_converter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from crudik.models.mentor import Mentor
 
 
 class MentorData(BaseModel):
-    id: UUID
-    full_name: str
-    contacts: list[str]
-    description: str | None = None
-    photo_url: str | None = None
+    id: UUID = Field(description="Id mentor")
+    full_name: str = Field(description="Mentor full name")
+    contacts: list[str] = Field(description="Mentor contatcs")
+    description: str | None = Field(description="Mentor description", default=None)
+    photo_url: str | None = Field(description="Mentor photo url", default=None)
 
 
 convert_mentor_to_dto = get_converter(Mentor, MentorData)
