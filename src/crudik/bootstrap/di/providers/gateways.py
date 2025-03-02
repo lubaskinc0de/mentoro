fd7c528e5d7faf2f8e94f3ce677089aeb4d07754
lubaskin <lubaskincorporation@gmail.com>
2025-03-02 22:31:30 +0300

    refactoring adapters

--- src/crudik/bootstrap/di/providers/gateways.py
+++ src/crudik/bootstrap/di/providers/gateways.py
@@ -1,11 +1,11 @@
 from dishka import Provider, Scope, WithParents, provide_all
 
-from crudik.adapters.db.access_token import AccessTokenGatewayImpl
-from crudik.adapters.db.mentor import MentorContactGatewayImpl, MentorGatewayImpl, MentorSkillGatewayImpl
-from crudik.adapters.db.mentoring_request import MentoringRequestGatewayImpl
-from crudik.adapters.db.review import ReviewGatewayImpl
-from crudik.adapters.db.student import StudentGatewayImpl
-from crudik.adapters.db.swiped_gateway import SwipedMentorGatewayImpl
+from crudik.adapters.gateway.access_token import AccessTokenGatewayImpl
+from crudik.adapters.gateway.mentor import MentorContactGatewayImpl, MentorGatewayImpl, MentorSkillGatewayImpl
+from crudik.adapters.gateway.mentoring_request import MentoringRequestGatewayImpl
+from crudik.adapters.gateway.review import ReviewGatewayImpl
+from crudik.adapters.gateway.student import StudentGatewayImpl
+from crudik.adapters.gateway.swiped_gateway import SwipedMentorGatewayImpl
 
 
 class GatewayProvider(Provider):
