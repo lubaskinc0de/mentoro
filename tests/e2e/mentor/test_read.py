1affad174066ec4b9eb124c585924b1bbd179318
lubaskin <lubaskincorporation@gmail.com>
2025-03-02 12:21:17 +0300

    read test

--- tests/e2e/mentor/test_read.py
+++ tests/e2e/mentor/test_read.py
@@ -1,4 +1,5 @@
 from crudik.adapters.test_api_gateway import TestApiGateway
+from crudik.application.data_model.mentor import MentorData
 from crudik.application.mentor.interactors.sign_up import SignUpMentorRequest
 
 
@@ -13,8 +14,11 @@ async def test_success_read(
     assert read_response.status_code == 200
     assert read_response.model is not None
 
+    expected = MentorData(**mentor.model_dump(), id=read_response.model.id)
+    assert expected == read_response.model
 
-async def test_unsuccess_unauthorizated(api_gateway: TestApiGateway) -> None:
+
+async def test_unsuccess_unauthorized(api_gateway: TestApiGateway) -> None:
     response = await api_gateway.read_mentor("")
     assert response.status_code == 401
     assert response.model is None
