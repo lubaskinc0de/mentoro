0df754c6c213b44aba58d116315d96be7c4b46c0
lubaskin <lubaskincorporation@gmail.com>
2025-03-02 09:47:07 +0300

    filler

---
+++ src/filler/fill_data.py
@@ -0,0 +1,43 @@
+import os
+
+import aiohttp
+from aiohttp import ClientSession
+
+from filler.test_gateway import TestApiGateway
+
+INTERESTS = [
+    "математика",
+    "русский язык",
+    "английский язык",
+    "немецкий язык",
+    "обществознание",
+    "программирование",
+    "python",
+    "java",
+    "sql",
+    "devops",
+    "mobile",
+    "ci/cd",
+    "матанализ",
+    "теорвер",
+    "чистая архитектура",
+    "embed",
+]
+
+base_names = ["Name"]
+
+
+async def fill_mentors(gateway: TestApiGateway) -> None: ...
+
+
+async def fill_students(gateway: TestApiGateway) -> None: ...
+
+
+async def fill_data() -> None:
+    async with ClientSession(
+        base_url=os.environ["EXTERNAL_API_URL"],
+        connector=aiohttp.TCPConnector(ssl=False),
+    ) as session:
+        gateway = TestApiGateway(session)
+        await fill_mentors(gateway)
+        await fill_students(gateway)
